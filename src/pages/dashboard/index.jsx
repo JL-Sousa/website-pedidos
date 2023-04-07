476ea9bf090cb79cb66ea12e219551ed0f004070
Jonyelson Lopes <jonyelson20@gmail.com>
2023-04-07 11:48:42 -0300

    Refatoracao no Dashboard para usar estados

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,49 +1,75 @@
+import { useEffect, useState } from "react";
 import { Grafico } from "../../components/Grafico";
 import { Navbar } from "../../components/Navbar";
 
 export function Dashboard() {
-  const dados_clientes = [
-    ["Clientes", "Vendas"],
-    ["Microsoft", 5200],
-    ["IBM", 4900],
-    ["Apple", 4200],
-    ["Facebook", 2140],
-    ["Google", 2140],
-  ];
+  const [dadosClientes, setDadosClientes] = useState([]);
+  const [dadosVendas, setDadosVendas] = useState([]);
+  const [dadosProdutos, setDadosProdutos] = useState([]);
+  const [dadosCidades, setDadosCidades] = useState([]);
 
-  const dados_vendas = [
-    ["Mes", "Vendas"],
-    ["1", 5200],
-    ["2", 4900],
-    ["3", 7200],
-    ["4", 4900],
-    ["5", 4900],
-    ["6", 1900],
-    ["7", 4900],
-    ["8", 4210],
-    ["9", 7630],
-    ["10", 5630],
-    ["11", 3630],
-    ["12", 10630],
-  ];
+  function graficoCliente() {
+    setDadosClientes([
+      ["Clientes", "Vendas"],
+      ["Microsoft", 5200],
+      ["IBM", 4900],
+      ["Apple", 4200],
+      ["Facebook", 2140],
+      ["Google", 2140],
+    ]);
+  }
 
-  const dados_produtos = [
-    ["Produto", "Vendas"],
-    ["Teclado", 5200],
-    ["Monitor", 4900],
-    ["HD", 4200],
-    ["Fone", 3210],
-    ["Webcam", 2140],
-  ];
+  function graficoVendas() {
+    setDadosVendas([
+      ["Mes", "Vendas"],
+      ["1", 5200],
+      ["2", 4900],
+      ["3", 7200],
+      ["4", 4900],
+      ["5", 4900],
+      ["6", 1900],
+      ["7", 4900],
+      ["8", 4210],
+      ["9", 7630],
+      ["10", 5630],
+      ["11", 3630],
+      ["12", 10630],
+    ]);
+  }
 
-  const dados_cidades = [
-    ["Cidade", "Vendas"],
-    ["Sao Paulo", 5200],
-    ["Rio de janeiro", 4900],
-    ["Campinas", 4200],
-    ["Curitiba", 3210],
-    ["Bauru", 2140],
-  ];
+  function graficoProdutos() {
+    setDadosProdutos([
+      ["Produto", "Vendas"],
+      ["Teclado", 5200],
+      ["Monitor", 4900],
+      ["HD", 4200],
+      ["Fone", 3210],
+      ["Webcam", 2140],
+    ]);
+  }
+
+  function graficoCidades() {
+    setDadosCidades([
+      ["Cidade", "Vendas"],
+      ["Sao Paulo", 5200],
+      ["Rio de janeiro", 4900],
+      ["Campinas", 4200],
+      ["Curitiba", 3210],
+      ["Bauru", 2140],
+    ]);
+  }
+
+  function montarGrafico() {
+    graficoCliente();
+    graficoVendas();
+    graficoProdutos();
+    graficoCidades();
+  }
+
+  useEffect(() => {
+    montarGrafico();
+  }, []);
+
   return (
     <>
       <Navbar />
@@ -59,7 +85,7 @@ export function Dashboard() {
             <Grafico
               titulo="Venda por clientes (top 5)"
               chartType="Bar"
-              dados={dados_clientes}
+              dados={dadosClientes}
               legenda={false}
             />
           </div>
@@ -67,7 +93,7 @@ export function Dashboard() {
             <Grafico
               titulo="Vendas anual"
               chartType="Line"
-              dados={dados_vendas}
+              dados={dadosVendas}
               legenda={false}
             />
           </div>
@@ -75,7 +101,7 @@ export function Dashboard() {
             <Grafico
               titulo="Venda por produto (top 5)"
               chartType="PieChart"
-              dados={dados_produtos}
+              dados={dadosProdutos}
               legenda={true}
             />
           </div>
@@ -83,7 +109,7 @@ export function Dashboard() {
             <Grafico
               titulo="Venda por cidade"
               chartType="BarChart"
-              dados={dados_cidades}
+              dados={dadosCidades}
               legenda={false}
             />
           </div>
